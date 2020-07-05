45048842d6114913e5d937425461ba93a12a8e9f
Grifano <sorlenk@gmail.com>
2020-07-05 20:23:27 +0200

    add files & folders

---
+++ #source/js/forms.js
@@ -0,0 +1,366 @@
+//FORMS
+// === Send form popup ===
+
+// === /Send form popup ===
+function forms() {
+	//FIELDS
+	$('input,textarea').focus(function () {
+		if ($(this).val() == $(this).attr('data-value')) {
+			$(this).addClass('focus');
+			$(this).parent().addClass('focus');
+			if ($(this).attr('data-type') == 'pass') {
+				$(this).attr('type', 'password');
+			};
+			$(this).val('');
+		};
+		removeError($(this));
+	});
+	$('input[data-value], textarea[data-value]').each(function () {
+		if (this.value == '' || this.value == $(this).attr('data-value')) {
+			if ($(this).hasClass('l') && $(this).parent().find('.form__label').length == 0) {
+				$(this).parent().append('<div class="form__label">' + $(this).attr('data-value') + '</div>');
+			} else {
+				this.value = $(this).attr('data-value');
+			}
+		}
+		if (this.value != $(this).attr('data-value') && this.value != '') {
+			$(this).addClass('focus');
+			$(this).parent().addClass('focus');
+			if ($(this).hasClass('l') && $(this).parent().find('.form__label').length == 0) {
+				$(this).parent().append('<div class="form__label">' + $(this).attr('data-value') + '</div>');
+			}
+		}
+
+		$(this).click(function () {
+			if (this.value == $(this).attr('data-value')) {
+				if ($(this).attr('data-type') == 'pass') {
+					$(this).attr('type', 'password');
+				};
+				this.value = '';
+			};
+		});
+		$(this).blur(function () {
+			if (this.value == '') {
+				if (!$(this).hasClass('l')) {
+					this.value = $(this).attr('data-value');
+				}
+				$(this).removeClass('focus');
+				$(this).parent().removeClass('focus');
+				if ($(this).attr('data-type') == 'pass') {
+					$(this).attr('type', 'text');
+				};
+			};
+			if ($(this).hasClass('vn')) {
+				formValidate($(this));
+			}
+		});
+	});
+	$('.form-input__viewpass').click(function (event) {
+		if ($(this).hasClass('active')) {
+			$(this).parent().find('input').attr('type', 'password');
+		} else {
+			$(this).parent().find('input').attr('type', 'text');
+		}
+		$(this).toggleClass('active');
+	});
+
+	//$('textarea').autogrow({vertical: true, horizontal: false});
+
+
+	//MASKS//
+	//'+7(999) 999 9999'
+	//'+38(999) 999 9999'
+	//'+375(99)999-99-99'
+	//'a{3,1000}' только буквы минимум 3
+	//'9{3,1000}' только цифры минимум 3
+	$.each($('input.phone'), function (index, val) {
+		$(this).attr('type', 'tel');
+		$(this).focus(function () {
+			$(this).inputmask('+7(999) 999 9999', {
+				clearIncomplete: true, clearMaskOnLostFocus: true,
+				"onincomplete": function () { maskclear($(this)); }
+			});
+			$(this).addClass('focus');
+			$(this).parent().addClass('focus');
+			$(this).parent().removeClass('err');
+			$(this).removeClass('err');
+		});
+	});
+	$('input.phone').focusout(function (event) {
+		maskclear($(this));
+	});
+	$.each($('input.num'), function (index, val) {
+		$(this).focus(function () {
+			$(this).inputmask('9{1,1000}', { clearIncomplete: true, placeholder: "", clearMaskOnLostFocus: true, "onincomplete": function () { maskclear($(this)); } });
+			$(this).addClass('focus');
+			$(this).parent().addClass('focus');
+			$(this).parent().removeClass('err');
+			$(this).removeClass('err');
+		});
+	});
+	$('input.num').focusout(function (event) {
+		maskclear($(this));
+	});
+	/*
+	$.each($('input.date'), function(index, val) {
+		$(this).focus(function(){
+			$(this).inputmask('dd.mm.yyyy',{
+				clearIncomplete: true,
+				placeholder:"_",
+				//yearrange:{'minyear':n-40,'maxyear':n},
+				clearMaskOnLostFocus: true,
+				"onincomplete": function(){maskclear($(this));},
+				"oncomplete": function(){
+					$(this).datepicker("setDate",$(this).val());
+				}
+			});
+			$(this).addClass('focus');
+			$(this).parents('.form-column').addClass('focus');
+			$(this).parent().addClass('focus');
+			$(this).parent().removeClass('err');
+			$(this).removeClass('err');
+		});
+		$(this).focusout(function(event) {
+			maskclear($(this));
+		});
+		$(this).datepicker({
+			dateFormat : "dd.mm.yy",
+			//yearRange: "1915:2015",
+			//defaultDate: '-18Y', 
+			//inDate: '-85Y', 
+			//maxDate: "0Y",
+			beforeShow :function(event){
+				$('.ui-datepicker').show();
+			},
+			onSelect:function(event){
+				if($(this).val()!=$(this).attr('data-value') && $(this).val()!=''){
+					$(this).addClass('focus');
+					$(this).parent().addClass('focus');
+					if($(this).hasClass('l') && $(this).parent().find('.form__label').length==0){
+						$(this).parent().append('<div class="form__label">'+$(this).attr('data-value')+'</div>');
+					}
+				}
+			}
+		});
+	});
+	*/
+	//RATING
+	$('.rating.edit .star').hover(function () {
+		var block = $(this).parents('.rating');
+		block.find('.rating__activeline').css({ width: '0%' });
+		var ind = $(this).index() + 1;
+		var linew = ind / block.find('.star').length * 100;
+		setrating(block, linew);
+	}, function () {
+		var block = $(this).parents('.rating');
+		block.find('.star').removeClass('active');
+		var ind = block.find('input').val();
+		var linew = ind / block.find('.star').length * 100;
+		setrating(block, linew);
+	});
+	$('.rating.edit .star').click(function (event) {
+		var block = $(this).parents('.rating');
+		var re = $(this).index() + 1;
+		block.find('input').val(re);
+		var linew = re / block.find('.star').length * 100;
+		setrating(block, linew);
+	});
+	$.each($('.rating'), function (index, val) {
+		var ind = $(this).find('input').val();
+		var linew = ind / $(this).parent().find('.star').length * 100;
+		setrating($(this), linew);
+	});
+	function setrating(th, val) {
+		th.find('.rating__activeline').css({ width: val + '%' });
+	}
+
+}
+forms();
+
+function digi(str) {
+	var r = str.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+	return r;
+}
+
+//VALIDATE FORMS
+$('form button[type=submit]').click(function () {
+	var er = 0;
+	var form = $(this).parents('form');
+	var ms = form.data('ms');
+	$.each(form.find('.req'), function (index, val) {
+		er += formValidate($(this));
+	});
+	if (er == 0) {
+		removeFormError(form);
+		/*
+			var messagehtml='';
+		if(form.hasClass('editprofile')){
+			var messagehtml='';
+		}
+		formLoad();
+		*/
+
+		//ОПТРАВКА ФОРМЫ
+		/*
+		function showResponse(html){
+			if(!form.hasClass('nomessage')){
+				showMessage(messagehtml);
+			}
+			if(!form.hasClass('noclear')){
+				clearForm(form);
+			}
+		}
+		var options={
+			success:showResponse
+		};
+			form.ajaxForm(options);
+		
+
+		setTimeout(function(){
+			if(!form.hasClass('nomessage')){
+				//showMessage(messagehtml);
+				showMessageByClass(ms);
+			}
+			if(!form.hasClass('noclear')){
+				clearForm(form);
+			}
+		},0);
+
+		return false;
+		*/
+		if (ms != null && ms != '') {
+			showMessageByClass(ms);
+			return false;
+		}
+	} else {
+		return false;
+	}
+});
+function formValidate(input) {
+	var er = 0;
+	var form = input.parents('form');
+	if (input.attr('name') == 'email' || input.hasClass('email')) {
+		if (input.val() != input.attr('data-value')) {
+			var em = input.val().replace(" ", "");
+			input.val(em);
+		}
+		if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.val())) || input.val() == input.attr('data-value')) {
+			er++;
+			addError(input);
+		} else {
+			removeError(input);
+		}
+	} else {
+		if (input.val() == '' || input.val() == input.attr('data-value')) {
+			er++;
+			addError(input);
+		} else {
+			removeError(input);
+		}
+	}
+	if (input.attr('type') == 'checkbox') {
+		if (input.prop('checked') == true) {
+			input.removeClass('err').parent().removeClass('err');
+		} else {
+			er++;
+			input.addClass('err').parent().addClass('err');
+		}
+	}
+	if (input.hasClass('name')) {
+		if (!(/^[А-Яа-яa-zA-Z-]+( [А-Яа-яa-zA-Z-]+)$/.test(input.val()))) {
+			er++;
+			addError(input);
+		}
+	}
+	if (input.hasClass('pass-2')) {
+		if (form.find('.pass-1').val() != form.find('.pass-2').val()) {
+			addError(input);
+		} else {
+			removeError(input);
+		}
+	}
+	return er;
+}
+function formLoad() {
+	$('.popup').hide();
+	$('.popup-message-body').hide();
+	$('.popup-message .popup-body').append('<div class="popup-loading"><div class="popup-loading__title">Идет загрузка...</div><div class="popup-loading__icon"></div></div>');
+	$('.popup-message').addClass('active').fadeIn(300);
+}
+function showMessageByClass(ms) {
+	$('.popup').hide();
+	popupOpen('message.' + ms, '');
+}
+function showMessage(html) {
+	$('.popup-loading').remove();
+	$('.popup-message-body').show().html(html);
+}
+function clearForm(form) {
+	$.each(form.find('.input'), function (index, val) {
+		$(this).removeClass('focus').val($(this).data('value'));
+		$(this).parent().removeClass('focus');
+		if ($(this).hasClass('phone')) {
+			maskclear($(this));
+		}
+	});
+}
+function addError(input) {
+	input.addClass('err');
+	input.parent().addClass('err');
+	input.parent().find('.form__error').remove();
+	if (input.hasClass('email')) {
+		var error = '';
+		if (input.val() == '' || input.val() == input.attr('data-value')) {
+			error = input.data('error');
+		} else {
+			error = input.data('error');
+		}
+		if (error != null) {
+			input.parent().append('<div class="form__error">' + error + '</div>');
+		}
+	} else {
+		if (input.data('error') != null && input.parent().find('.form__error').length == 0) {
+			input.parent().append('<div class="form__error">' + input.data('error') + '</div>');
+		}
+	}
+	if (input.parents('.select-block').length > 0) {
+		input.parents('.select-block').parent().addClass('err');
+		input.parents('.select-block').find('.select').addClass('err');
+	}
+}
+function addErrorByName(form, input__name, error_text) {
+	var input = form.find('[name="' + input__name + '"]');
+	input.attr('data-error', error_text);
+	addError(input);
+}
+function addFormError(form, error_text) {
+	form.find('.form__generalerror').show().html(error_text);
+}
+function removeFormError(form) {
+	form.find('.form__generalerror').hide().html('');
+}
+function removeError(input) {
+	input.removeClass('err');
+	input.parent().removeClass('err');
+	input.parent().find('.form__error').remove();
+
+	if (input.parents('.select-block').length > 0) {
+		input.parents('.select-block').parent().removeClass('err');
+		input.parents('.select-block').find('.select').removeClass('err').removeClass('active');
+		//input.parents('.select-block').find('.select-options').hide();
+	}
+}
+function removeFormErrors(form) {
+	form.find('.err').removeClass('err');
+	form.find('.form__error').remove();
+}
+function maskclear(n) {
+	if (n.val() == "") {
+		n.inputmask('remove');
+		if (!n.hasClass('l')) {
+			n.val(n.attr('data-value'));
+		}
+		n.removeClass('focus');
+		n.parent().removeClass('focus');
+	}
+}
